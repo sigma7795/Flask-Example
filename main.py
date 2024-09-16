c81a3a724bbbd4cb5e1d043670f7937d43758860
Damon <7795@balcarras.gloucs.sch.uk>
2024-09-16 14:30:28 +0000

    created signup and login

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'THISISABADKEY'
 
 
+##routing
+@app.route('/')
+def home():
+    return render_template('index.html') #names webpage what you want
 
-
+@app.route('/signup')
+def signup():
+    return
 
 
 
